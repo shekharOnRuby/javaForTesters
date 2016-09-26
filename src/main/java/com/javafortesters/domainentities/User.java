c8fb937c659b53f02cdc7aaa631a19ea05b78d85
Shekhar Karande <shekhar.karande@gamesys.co.uk>
2016-09-26 20:22:51 +0100

    Commiting my java code from IDE

--- src/main/java/com/javafortesters/domainentities/User.java
+++ src/main/java/com/javafortesters/domainentities/User.java
@@ -4,4 +4,27 @@ package com.javafortesters.domainentities;
  * Created by shekhar.karande on 13/09/2016.
  */
 public class User {
+    private String username;
+    private String password;
+
+    public User(){
+        this("username","password");
+    }
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+
+    }
+
+    public String getUserName(){
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
+++ src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -3,6 +3,8 @@ package com.javafortesters.chap006domainentities.examples;
 import com.javafortesters.domainentities.User;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by shekhar.karande on 13/09/2016.
  */
@@ -10,5 +12,20 @@ public class UserTest {
     @Test
     public void canConstructANewUser(){
         User user = new User();
+    }
+    @Test
+    public void userHasDefaultUserNameAndPassword(){
+        User user = new User("admin","pA55w0rD");
+
+        assertEquals("default username expected","admin",user.getUserName());
+
+        assertEquals("default password expected","pA55w0rD",user.getPassword());
+    }
+
+    @Test
+    public void canSetUserPassword(){
+        User user = new User();
+        user.setPassword("PaZZwor6");
+        assertEquals("password expected","PaZZwor6",user.getPassword());
     }
 }
